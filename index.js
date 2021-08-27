1383451101a62b5f693775dd47bb8f93c95d3c52
Funnbot <22226942+funnbot@users.noreply.github.com>
2021-08-26 21:21:27 -0700

    reorganize

--- index.js
+++ index.js
@@ -26,21 +26,30 @@ class Doc {
 
     push(str) { this.#elements.push(str); return this; }
 
-    join() { return this.#elements.join("\n"); }
+    toString() { return this.#elements.join("\n") }
 }
 
-// a real number represented by a/b, it is rational if a and b are integers, irrational is difficult and we wont go there
+// a real number represented by a/b, irrational is difficult and we wont go there
 // if b is one it is a integer
 class Real {
+    #a = 0; #b = 1;
 
+    constructor(a, b = 1) { this.#a = a; }
+    get isInteger() {  return this.#b === 1 && Number.isInteger(this.#a); }
+    toString() { return (this.#a / this.#b).toString(); }
 }
 
 // Make a matrix class that will format into a KaTeX matrix
 // Row major, which means vals[column][row]
 class Matrix {
     #vals = [];
-}
 
+    constructor(vals) { this.#vals = vals }
+    toString() {
+        const katex = `\\begin{bmatrix}${this.#vals.map(a => a.join("&")).join("\\\\")}\\end{bmatrix}`
+        return math(katex); // katex to html;
+    }
+}
 // //{step}, //{require}, //{title}, //{sub}, //{p}
 
 // Includes the actual js file, and gets all of the exported functions
@@ -58,9 +67,9 @@ const code = str => Elem.pre(hl.highlight(str, { language: "javascript" }).value
 
 const math = str => katex.renderToString(str, { throwOnError: false, output: "html" });
 
-const template = fs.readFileSync(path.resolve(__dirname, path.join("html", "template.html")));
+const template = fs.readFileSync(path.resolve(__dirname, path.join("resources", "template.html")));
 
-const outputFile = "html/index.html";
+const outputFile = "build/index.html";
 
 const templateInsert = "<TEMPLATE_INSERT>";
 const insert = template.findIndex((v, i, a) => v === templateInsert.charCodeAt(0) && a.subarray(i + 1, i + templateInsert.length).toString() === templateInsert.slice(1));
@@ -71,8 +80,8 @@ doc.push(Elem.h1("Below the valley"));
 doc.push(Elem.p("paragraph of stuff would go here"));
 doc.push(code("const js = 10\njs += 2;\n"));
 doc.push(math(`\\begin{bmatrix}a & b \\\\c & d\\end{bmatrix}`));
-doc.push(math(`= \\begin{bmatrix}a & b \\\\c & d\\end{bmatrix}`));
+doc.push(new Matrix([[1, 2, 3], ["a", "c", "b"], ["x", 0.022, 1.2]]));
 doc.push(Elem.h2("Credits"));
 
-const output = template.subarray(0, insert) + doc.toString() + template.subarray(insert + templateInsert.length, template.length);
+const output = template.subarray(0, insert).toString() + doc.toString() + template.subarray(insert + templateInsert.length, template.length).toString();
 fs.writeFileSync(outputFile, output);
