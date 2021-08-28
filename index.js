6fbafb686712f61c8cd44ee76b15da66d14b5251
Funnbot <22226942+funnbot@users.noreply.github.com>
2021-08-28 00:18:58 -0700

    somewhat functioning

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const katex = require("katex");
 const fs = require("fs");
+var pdf = require('html-pdf');
+const rewire = require("rewire");
 const path = require("path");
 const hl = require("highlight.js/lib/core");
+const jsml = require("./lib/jsml.js");
+const { execSync } = require("child_process");
 hl.registerLanguage("javascript", require("highlight.js/lib/languages/javascript"));
 
 class Elem {
@@ -35,8 +39,8 @@ class Real {
     #a = 0; #b = 1;
 
     constructor(a, b = 1) { this.#a = a; }
-    isInteger() {  return this.#b === 1 && Number.isInteger(this.#a); }
     toString() { return (this.#a / this.#b).toString(); }
+    valueOf() { return this.#a / this.#b; }
 }
 
 // Make a matrix class that will format into a KaTeX matrix
@@ -47,7 +51,7 @@ class Matrix {
     constructor(vals) { this.#vals = vals }
     toString() {
         const katex = `\\begin{bmatrix}${this.#vals.map(a => a.join("&")).join("\\\\")}\\end{bmatrix}`
-        return math(katex); // katex to html;
+        return katex;
     }
 }
 // //{step}, //{require}, //{title}, //{sub}, //{p}
@@ -57,16 +61,17 @@ class Matrix {
 // Add a way to include steps inside the function, that are notated with mljs comments (//>) that will output a certain step
 // Include a library that has a step function, that excepts certain formats
 // stepImply(output) //{step} 
-/**
- * 
- * @param {Buffer} file 
- */
 function parse(filepath) {
     const file = fs.readFileSync(filepath, "utf-8").toString();
-    const fileCode = require(path.resolve(process.cwd(), filepath));
+    const fileCode = rewire(path.resolve(process.cwd(), filepath));
+    fileCode.__set__({
+        stepInput: jsml.stepInput,
+        stepEqual: jsml.stepEqual,
+    })
 
     let text = [];
     let code = {};
+
     let i = 0;
     while (i < file.length) {
         let char = file[i];
@@ -83,10 +88,10 @@ function parse(filepath) {
             const op = peek();
             i += 2; // skip the op
             if (op === "/") text.push({ style: "paragraph", str: readTill() });
-            else if (op === "t") text.push({style: "title", str: readTill() });
-            else if (op === "s") text.push({style: "subtitle", str: readTill() });
-            else if (op === "i") text.push({style: "include", data: parse(readTill())})
-            else if (op === "r") text.push({style: "run", code: readTill() })
+            else if (op === "t") text.push({ style: "title", str: readTill() });
+            else if (op === "s") text.push({ style: "subtitle", str: readTill() });
+            else if (op === "i") text.push({ style: "include", data: parse(readTill()) })
+            else if (op === "r") text.push({ style: "run", code: readTill() })
             else if (op === "c") {
                 const funcName = readTill();
                 i++; // skip newline
@@ -104,56 +109,99 @@ function parse(filepath) {
                     }
                     i++;
                 }
-                text.push({style: "code", funcName})
+                text.push({ style: "code", funcName })
                 code[funcName] = {
                     str: file.slice(start, end + 1),
-                    func: fileCode[funcName],
+                    func: fileCode.__get__(funcName),
                 };
             }
         }
         i++;
     }
-    return {text, code};
+    return { text, code };
 }
 
-const highlight = str => hl.highlight(str, { language: "javascript" }).value
-
-function display({text, code}) {
+function display({ text, code }) {
     let doc = new Doc();
     for (let i = 0; i < text.length; i++) {
         const t = text[i];
         if (t.style === "title") doc.push(Elem.h1(t.str));
         else if (t.style === "subtitle") doc.push(Elem.h2(t.str));
-        else if (t.style === "paragraph") doc.push(Elem.p(t.str));
+        else if (t.style === "paragraph") {
+            let fmt = "";
+            let ii = 0;
+            while (ii < t.str.length) {
+                const peek = (amt = 1) => (ii + amt) < t.str.length ? t.str[ii + amt] : null;
+                const readTill = (end) => {
+                    let buf = t.str[ii];
+                    while (peek() !== null && peek() !== end) buf += t.str[++ii];
+                    return buf;
+                }
+                if (t.str[ii] === "m" && peek() === "`") {
+                    ii += 2;
+                    fmt += math(readTill("`"));
+                    ii += 2;
+                } else if (t.str[ii] === "c" && peek() === "`") {
+                    ii += 2;
+                    fmt += `<code>${highlight(readTill("`"))}</code>`;
+                    ii += 2;
+                } else fmt += t.str[ii++];
+            }
+            doc.push(Elem.p(fmt));
+        }
         else if (t.style === "code") doc.push(new Elem("pre").inner(highlight(code[t.funcName].str)));
-        else if (t.style === "include") doc.push(display(t.data))
-        else if (t.style === "run") doc.push(new Elem("code").inner(highlight(t.code)))
+        else if (t.style === "include") doc.push(display(t.data).toString())
+        else if (t.style === "run") {
+            const callRegex = /^([\w\d_-]+)\((.*)\);?$/;
+            const callParsed = callRegex.exec(t.code);
+            const [_, funcName, argsText] = callParsed;
+            try {
+                var args = eval("[" + argsText + "]");
+            } catch (e) {
+                doc.push(new Elem("code").prop("style", "font-color: red;").inner(e.message));
+                continue;
+            }
+            code[funcName].func(...args);
+            let elem;
+            while ((elem = jsml._steps[funcName].shift()) !== undefined) {
+                let delim = "";
+                if (elem.type === "equal") delim = "=";
+                else if (elem.type === "imply") delim = "=>";
+
+                doc.push(math(delim + elem.obj.toString()));
+            }
+        }
     }
     return doc;
 }
 
+const highlight = str => hl.highlight(str, { language: "javascript" }).value
 const math = str => katex.renderToString(str, { throwOnError: false, output: "html" });
 
 const template = fs.readFileSync(path.resolve(__dirname, path.join("resources", "template.html")));
-
-const outputFile = "build/index.html";
-
 const templateInsert = "<TEMPLATE_INSERT>";
 const insert = template.findIndex((v, i, a) => v === templateInsert.charCodeAt(0) && a.subarray(i + 1, i + templateInsert.length).toString() === templateInsert.slice(1));
 
-console.log(parse("input.js"));
-const p = parse("input.js");
-const d = display(p);
+const inputFile = process.argv[2];
+let inputParsed = path.parse(inputFile);
 
 
-let doc = new Doc();
-// doc.push(Elem.h1("Title"));
-// doc.push(Elem.h1("Below the valley"));
-// doc.push(Elem.p("paragraph awd inline code of stuff would go here"));
-// doc.push(code("const js = 10\njs += 2;\n"));
-// doc.push(math(`\\begin{bmatrix}a & b \\\\c & d\\end{bmatrix}`));
-// doc.push(new Matrix([[1, 2, 3], ["a", "c", "b"], ["x", 0.022, 1.2]]));
-// doc.push(Elem.h2("Credits"));
+const outputFile = path.format(inputParsed);
+console.log(outputFile);
 
-const output = template.subarray(0, insert).toString() + d.toString() + template.subarray(insert + templateInsert.length, template.length).toString();
-fs.writeFileSync(outputFile, output);
+const parsedFile = parse(inputFile);
+
+const renderedFile = display(parsedFile);
+const output = template.subarray(0, insert).toString() + renderedFile.toString() + template.subarray(insert + templateInsert.length, template.length).toString();
+//fs.writeFileSync(outputFile, output);
+
+// fs.mkdirSync("build/styles", {recursive: true});
+// const distrib = path.resolve(__dirname, "distrib");
+// if (!fs.existsSync(distrib + "/", "build/", )) fs.cpSync(distrib + "/", "build/", {recursive: true});
+
+//var html = fs.readFileSync('./build/index.html', 'utf8');
+var options = { localUrlAccess: true, base: "file://" + __dirname + "/distrib/" };
+pdf.create(output, options).toFile("./" + inputParsed.name + ".pdf", function(err, res) {
+  if (err) return console.log(err);
+  console.log("Finished.");
+});
---
+++ jsml-syntax-highlighting/README.md
@@ -0,0 +1,3 @@
+# jsml-syntax-highlighting
+
+Injected theme for editing jsml in js files.
